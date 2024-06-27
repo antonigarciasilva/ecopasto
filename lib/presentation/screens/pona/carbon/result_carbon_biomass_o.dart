e1c72d89b3e89464985490509a411a21fe273b5d
antoni <garciasilvaantoni@gmail.com>
2024-06-27 12:19:08 -0500

    result whit img ok in pona

--- lib/presentation/screens/pona/carbon/result_carbon_biomass_o.dart
+++ lib/presentation/screens/pona/carbon/result_carbon_biomass_o.dart
@@ -17,15 +17,31 @@ class ResultCarbonBiomassO extends StatelessWidget {
     }
   }
 
+  //Vamos a insertar imgs
+  String getImagePath(String level) {
+    switch (level) {
+      case 'Bajo':
+        return 'assets/result/bajo.jpg';
+      case 'Medio':
+        return 'assets/result/medio.jpg';
+      case 'Excelente':
+        return 'assets/result/excelente.jpg';
+      default:
+        return 'assets/result/medio.jpg';
+    }
+  }
+
   //Vamos a definir las recomendaciones
   String getRecommendationsO(String level) {
     switch (level) {
       case 'Bajo':
         return 'Recomendaciones para mejorar:\n'
+            '\n'
             '1. Aumentar la cantidad de árboles \n '
             '2. Implementar técnicas de manejo sostenible';
       case 'Medio':
         return 'Recomendaciones para mejorar: \n'
+            '\n'
             '1. Mantener prácticas actuales \n'
             '2. Considerar aumentar áreas de silvopastoreo';
       case 'Excelente':
@@ -65,53 +81,78 @@ class ResultCarbonBiomassO extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Resultado de cálculo'),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Text(
-                'El carbono total en la biomasa es de: ${resultCarbonBiomassO.toStringAsFixed(2)} Tn/ha',
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 20),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Nivel de carbono: $level',
-                style: TextStyle(fontSize: 24, color: levelColor),
-                textAlign: TextAlign.justify,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                getRecommendationsO(level),
-                style: const TextStyle(fontSize: 18),
-                textAlign: TextAlign.justify,
-              ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
 
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                getNextMeasuremetTimeO(level),
-                style:
-                    const TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
-                textAlign: TextAlign.justify,
-              ),
+                  SizedBox(child: Image.asset(getImagePath(level))),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'El carbono total en la biomasa es de: ${resultCarbonBiomassO.toStringAsFixed(2)} Tn/ha',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Nivel de carbono: $level',
+                    style: TextStyle(fontSize: 24, color: levelColor),
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    getRecommendationsO(level),
+                    style: const TextStyle(fontSize: 16),
+                    textAlign: TextAlign.justify,
+                  ),
 
-              //Botón para aceptar
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const CarbonPonaScreen()));
-                  },
-                  child: const Text('Aceptar'))
-            ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    getNextMeasuremetTimeO(level),
+                    style: const TextStyle(
+                        fontSize: 15, fontStyle: FontStyle.italic),
+                    textAlign: TextAlign.justify,
+                  ),
+
+                  const SizedBox(
+                    height: 20,
+                  ),
+
+                  //Botón para aceptar
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              WidgetStateProperty.all<Color>(Colors.green)),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const CarbonPonaScreen()));
+                      },
+                      child: const Text(
+                        'Aceptar',
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ))
+                ],
+              ),
+            ),
           ),
         ),
       ),
